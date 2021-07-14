2d2010272f0769a4f73b239bcf74ff98e38b7bf3
louis09392003 <louis09392003@gmail.com>
2021-07-15 01:17:07 +0800

    recursion prompt finished

--- src/recursion.js
+++ src/recursion.js
@@ -280,6 +280,18 @@ var countValuesInObj = function(obj, value) {
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+  if (!(typeof(obj)==='object' && obj !== null)){
+    return obj;
+  } else {
+      if( obj[oldKey] !== undefined) { //if find target key
+        delete Object.assign(obj, {[newKey]: obj[oldKey] })[oldKey];
+      }
+
+      for ( var key in obj) {
+         replaceKeysInObj(obj[key], oldKey, newKey);
+      }
+    }
+    return obj;
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
